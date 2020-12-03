e40d0bdc9ffa3e534f9e8864ee808425c2352286
Gunpriya Sarpate <sarpatepriya@gmail.com>
2020-12-03 15:41:34 +0530

    Updates

---
+++ precache-manifest.bb918c7e2707b38ec0f0e4198b9eec56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3dc33e6e70d0c88106cd638bbc1fa65",
+    "url": "/idcard-generator/index.html"
+  },
+  {
+    "revision": "d8198474feec54cd3f06",
+    "url": "/idcard-generator/static/css/main.929694cb.chunk.css"
+  },
+  {
+    "revision": "dcdaa5af4bb941f6d75b",
+    "url": "/idcard-generator/static/js/2.a09fd31d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/idcard-generator/static/js/2.a09fd31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b44b14154d99b45e581",
+    "url": "/idcard-generator/static/js/3.d424fabf.chunk.js"
+  },
+  {
+    "revision": "4cfe1c4f9a3582e4e396e40b40a06bf5",
+    "url": "/idcard-generator/static/js/3.d424fabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4745dffdce12386617c5",
+    "url": "/idcard-generator/static/js/4.4e41b9b2.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/idcard-generator/static/js/4.4e41b9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8198474feec54cd3f06",
+    "url": "/idcard-generator/static/js/main.dea61110.chunk.js"
+  },
+  {
+    "revision": "da5e1fec22a12c8ba4e7",
+    "url": "/idcard-generator/static/js/runtime-main.b3bd44d4.js"
+  }
+]);
